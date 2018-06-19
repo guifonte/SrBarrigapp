ebe191e51708a637307c15b08033a285cc9f87ee
Guifonte <guilhermenishifonte@gmail.com>
2018-06-18 22:40:03 -0300

    add redirect function, local storage and automatic authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,20 @@ import { SpendingListComponent } from './spendings/spending-list/spending-list.c
 import { SpendingCreateComponent } from './spendings/spending-create/spending-create.component';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   { path: '', component: SpendingListComponent },
-  { path: 'create', component: SpendingCreateComponent },
-  { path: 'edit/:spendingId', component: SpendingCreateComponent },
+  { path: 'create', component: SpendingCreateComponent, canActivate: [AuthGuard] },
+  { path: 'edit/:spendingId', component: SpendingCreateComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: 'signup', component: SignupComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [AuthGuard]
 })
 
 export class AppRoutingModule {}
